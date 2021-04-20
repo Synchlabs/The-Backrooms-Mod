ef1bc1bf3d81d5831b818882901cf2e189a0be6c
Valiec <valiec2019@gmail.com>
2021-04-19 20:59:00 -0400

    Made Redrooms generate.

--- src/main/java/net/synchlabs/backrooms/init/BackroomsBiomes.java
+++ src/main/java/net/synchlabs/backrooms/init/BackroomsBiomes.java
@@ -11,18 +11,21 @@ import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
+import net.synchlabs.backrooms.world.biome.RedRoomsBiome;
 
 public class BackroomsBiomes {
 
 	private static final Map<Identifier, Biome> BIOMES = new LinkedHashMap<>();
 
 	public static final RegistryKey<Biome> LEVEL0 = add("level0", Level0Biome.create());
+	public static final RegistryKey<Biome> REDROOMS_LEVEL0 = add("redrooms_level0", RedRoomsBiome.create());
 
 	public static void init() {
 		for (Identifier id : BIOMES.keySet()) {
 			Registry.register(BuiltinRegistries.BIOME, id, BIOMES.get(id));
 		}
 		Level0.NOISE_POINTS.put(LEVEL0, new Biome.MixedNoisePoint(0.0F, 0.0F, 0.0F, 0.0F, 0.0F));
+		Level0.NOISE_POINTS.put(REDROOMS_LEVEL0, new Biome.MixedNoisePoint(0.0F, 0.0F, 0.0F, 0.0F, 0.0F));
 	}
 
 	private static RegistryKey<Biome> add(String s, Biome b) {
