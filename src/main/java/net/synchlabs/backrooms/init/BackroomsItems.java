b394573cb2d00e4275b4d37374ffc38b4c3af28c
Valiec <valiec2019@gmail.com>
2021-04-20 18:05:19 -0400

    Added lang entries.

--- src/main/java/net/synchlabs/backrooms/init/BackroomsItems.java
+++ src/main/java/net/synchlabs/backrooms/init/BackroomsItems.java
@@ -16,8 +16,8 @@ public class BackroomsItems {
 
 	private static final Map<Identifier, Item> ITEMS = new LinkedHashMap<>();
 
-	public static final Item ARROWED_PATTERN = add("arrowed_pattern", new WallpaperPatternItem(BackroomsLoomPatterns.ARROWED, WallpaperType.ARROWED, new FabricItemSettings().group(ItemGroup.MISC)));
-	public static final Item LINED_PATTERN = add("lined_pattern", new WallpaperPatternItem(BackroomsLoomPatterns.LINED, WallpaperType.LINED, new FabricItemSettings().group(ItemGroup.MISC)));
+	public static final Item PATTERNED_PATTERN = add("patterned_pattern", new WallpaperPatternItem(BackroomsLoomPatterns.ARROWED, WallpaperType.ARROWED, new FabricItemSettings().group(ItemGroup.MISC)));
+	public static final Item STRIPED_PATTERN = add("striped_pattern", new WallpaperPatternItem(BackroomsLoomPatterns.LINED, WallpaperType.LINED, new FabricItemSettings().group(ItemGroup.MISC)));
 	public static final Item DOTTED_PATTERN = add("dotted_pattern", new WallpaperPatternItem(BackroomsLoomPatterns.DOTTED, WallpaperType.DOTTED, new FabricItemSettings().group(ItemGroup.MISC)));
 	public static final Item BLANK_PATTERN = add("blank_pattern", new WallpaperPatternItem(BackroomsLoomPatterns.BLANK, WallpaperType.BLANK, new FabricItemSettings().group(ItemGroup.MISC)));
 
