f471646b70a6a722a7300859311895b0ed4368c1
Synchlabs <v0pt2.0@gmail.com>
2021-05-02 07:29:30 -0400

    Fixing some things

--- src/main/java/net/synchlabs/backrooms/client/BackroomsClient.java
+++ src/main/java/net/synchlabs/backrooms/client/BackroomsClient.java
@@ -2,10 +2,16 @@ package net.synchlabs.backrooms.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.ludocrypt.exdimapi.api.ExtraSkiesApi;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.SkyProperties;
+import net.minecraft.util.Identifier;
 import net.synchlabs.backrooms.init.BackroomsBlocks;
+import net.synchlabs.backrooms.world.Level0;
 
-public class BackroomsClient implements ClientModInitializer {
+import java.util.Map;
+
+public class BackroomsClient implements ClientModInitializer, ExtraSkiesApi {
 
 	@Override
 	public void onInitializeClient() {
@@ -13,4 +19,8 @@ public class BackroomsClient implements ClientModInitializer {
 		BlockRenderLayerMap.INSTANCE.putBlock(BackroomsBlocks.OFFICE_DOOR, RenderLayer.getTranslucent());
 	}
 
+	@Override
+	public void registerModSkies(Map<Identifier, SkyProperties> map) {
+		new Level0.Level0Sky();
+	}
 }
